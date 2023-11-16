3673734e84eacc89d4cca5fcba8365557f31cd7c
matavock <fifermas@gmail.com>
2023-11-17 01:44:57 +0300

    Modify main.py

--- main.py
+++ main.py
@@ -1,18 +1,38 @@
 import sys
-from PyQt5 import uic
+from PyQt5 import QtWidgets, QtCore
 from PyQt5.QtWidgets import QApplication, QMainWindow
 from PyQt5.QtGui import QPainter, QColor
 from random import randint
 
 
-class MyWidget(QMainWindow):
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(800, 600)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.pushButton = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton.setGeometry(QtCore.QRect(350, 260, 93, 28))
+        self.pushButton.setObjectName("pushButton")
+        MainWindow.setCentralWidget(self.centralwidget)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.pushButton.setText(_translate("MainWindow", "Draw"))
+
+
+class MyWidget(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
-        uic.loadUi('Git-and-yellow-circles\\UI.ui', self)  # Загрузка интерфейса из файла UI.ui
+        self.setupUi(self)
         self.pushButton.clicked.connect(self.draw)
 
     def draw(self):
-        self.update()  # Обновление окна, вызывает перерисовку и срабатывание paintEvent
+        self.update()
 
     def paintEvent(self, event):
         qp = QPainter()
@@ -21,14 +41,13 @@ class MyWidget(QMainWindow):
         qp.end()
 
     def drawCircles(self, qp):
-        qp.setBrush(QColor(255, 255, 0))  # Цвет кисти - желтый
         for _ in range(10):
-            d = randint(10, 50)  # Случайный диаметр окружности
-            # Случайное положение окружности по x
+            qp.setBrush(
+                QColor(randint(0, 255), randint(0, 255), randint(0, 255)))
+            d = randint(10, 50)
             x = randint(0, self.width() - d)
-            # Случайное положение окружности по y
             y = randint(0, self.height() - d)
-            qp.drawEllipse(x, y, d, d)  # Рисование окружности
+            qp.drawEllipse(x, y, d, d)
 
 
 app = QApplication(sys.argv)
